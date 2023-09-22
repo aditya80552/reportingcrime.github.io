17ae76cf6d9d87f825059370185c27c0d35c937d
aditya80552 <51125486+aditya80552@users.noreply.github.com>
2023-09-22 10:10:28 +0530

    Update script.js
    Signed-off-by: aditya80552 <51125486+aditya80552@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Front end code
 const form = document.querySelector('form');
 
 form.addEventListener('submit', async (event) => {
@@ -6,8 +7,8 @@ form.addEventListener('submit', async (event) => {
   const name = form.querySelector('input[name="name"]').value;
   const email = form.querySelector('input[name="email"]').value;
   const crime = form.querySelector('input[name="crime"]').value;
-const department = form.querySelector('input[name="department"]').value;
-const description = form.querySelector('input[name="description"]').value;
+  const description = form.querySelector('input[name="description"]').value;
+  const department = form.querySelector('input[name="department"]').value;
 
   const response = await fetch('/report', {
     method: 'POST',
@@ -17,7 +18,9 @@ const description = form.querySelector('input[name="description"]').value;
     body: JSON.stringify({
       name,
       email,
-      crime
+      crime,
+      description,
+      department
     })
   });
 
@@ -26,4 +29,30 @@ const description = form.querySelector('input[name="description"]').value;
   } else {
     alert('Error reporting crime!');
   }
+});
+
+// Backend code
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/report', async (req, res) => {
+  const crime = new Crime({
+    name: req.body.name,
+    email: req.body.email,
+    crime: req.body.crime,
+    description: req.body.description,
+    department: req.body.department
+  });
+
+  await crime.save();
+
+  res.send('Crime reported successfully!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
 });
