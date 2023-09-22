2ff54bf805b196eec3d47219e4b412c42141cfe3
aditya80552 <51125486+aditya80552@users.noreply.github.com>
2023-09-22 11:17:00 +0530

    Update node.js
    Signed-off-by: aditya80552 <51125486+aditya80552@users.noreply.github.com>

--- node.js
+++ node.js
@@ -36,13 +36,4 @@ app.post('/report', async (req, res) => {
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
 });
-const express = require('express');
-
-const app = express();
-
-app.use(express.static('.'));
-
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
 
