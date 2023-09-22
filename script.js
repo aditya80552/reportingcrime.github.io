d6657f8e56b072a00a4580f763cb6c152f810d37
aditya80552 <51125486+aditya80552@users.noreply.github.com>
2023-09-22 11:30:44 +0530

    Update script.js
    Signed-off-by: aditya80552 <51125486+aditya80552@users.noreply.github.com>

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ form.addEventListener('submit', async (event) => {
   const name = form.querySelector('input[name="name"]').value;
   const email = form.querySelector('input[name="email"]').value;
   const crime = form.querySelector('input[name="crime"]').value;
-const department = form.querySelector('input[name="department"]').value;
-const description = form.querySelector('input[name="description"]').value;
 
   const response = await fetch('/report', {
     method: 'POST',
@@ -17,9 +15,7 @@ const description = form.querySelector('input[name="description"]').value;
     body: JSON.stringify({
       name,
       email,
-      crime,
-      department,
-      description
+      crime
     })
   });
 
@@ -29,5 +25,3 @@ const description = form.querySelector('input[name="description"]').value;
     alert('Error reporting crime!');
   }
 });
-
-
