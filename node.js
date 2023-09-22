e1e200fb351b6fc8f1074df4116a6a74611e1729
aditya80552 <51125486+aditya80552@users.noreply.github.com>
2023-09-22 11:31:30 +0530

    Update node.js
    Signed-off-by: aditya80552 <51125486+aditya80552@users.noreply.github.com>

--- node.js
+++ node.js
@@ -1,31 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/women-crime-reporting');
-
-const crimeSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  crime: String,
-department: String,
- description: String
-
-});
-
-const Crime = mongoose.model('Crime', crimeSchema);
-
 app.post('/report', async (req, res) => {
   const crime = new Crime({
     name: req.body.name,
     email: req.body.email,
-    crime: req.body.crime,
-     department: req.body.department,
-    description: req.body.description
+    crime: req.body.crime
   });
 
   await crime.save();
