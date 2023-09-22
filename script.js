c5eef519581f43cb23f215acb839f2371d64a96c
aditya80552 <51125486+aditya80552@users.noreply.github.com>
2023-09-22 10:13:07 +0530

    Update script.js
    Signed-off-by: aditya80552 <51125486+aditya80552@users.noreply.github.com>

--- script.js
+++ script.js
@@ -31,28 +31,3 @@ form.addEventListener('submit', async (event) => {
   }
 });
 
-// Backend code
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-
-app.use(bodyParser.json());
-
-app.post('/report', async (req, res) => {
-  const crime = new Crime({
-    name: req.body.name,
-    email: req.body.email,
-    crime: req.body.crime,
-    description: req.body.description,
-    department: req.body.department
-  });
-
-  await crime.save();
-
-  res.send('Crime reported successfully!');
-});
-
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
