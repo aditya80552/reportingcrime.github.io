8a95f57d0986a8c761974cc1fcb828bc82e9930f
aditya80552 <51125486+aditya80552@users.noreply.github.com>
2023-09-22 09:52:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const name = form.querySelector('input[name="name"]').value;
+  const email = form.querySelector('input[name="email"]').value;
+  const crime = form.querySelector('input[name="crime"]').value;
+const department = form.querySelector('input[name="department"]').value;
+const description = form.querySelector('input[name="description"]').value;
+
+  const response = await fetch('/report', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name,
+      email,
+      crime
+    })
+  });
+
+  if (response.ok) {
+    alert('Crime reported successfully!');
+  } else {
+    alert('Error reporting crime!');
+  }
+});
